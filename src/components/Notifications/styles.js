a13106f84093d8e73fbda099c91af3d745f32a95
Marcus Reaiche <marcusreaiche@gmail.com>
2020-02-13 22:01:01 -0300

    Styling notifications

---
+++ src/components/Notifications/styles.js
@@ -0,0 +1,99 @@
+import styled, { css } from "styled-components";
+import { lighten } from "polished";
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const Badge = styled.button`
+  background: none;
+  border: 0;
+  position: relative;
+
+  ${props =>
+    props.hasUnread &&
+    css`
+      &::after {
+        position: absolute;
+        top: 0;
+        right: 0;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: #ff892e;
+        content: "";
+      }
+    `}
+`;
+
+export const NotificationList = styled.div`
+  position: absolute;
+  width: 260px;
+  left: calc(50% - 130px);
+  top: calc(100% + 30px);
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  padding: 15px 5px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: -20px;
+    left: calc(50% - 20px);
+    height: 0;
+    width: 0;
+    border-bottom: 20px solid rgba(0, 0, 0, 0.6);
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+  }
+`;
+
+export const Notification = styled.div`
+  color: #fff;
+
+  & + div {
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  p {
+    font-size: 13px;
+    line-height: 18px;
+  }
+
+  time {
+    font-size: 12px;
+    opacity: 0.6;
+  }
+
+  button {
+    font-size: 12px;
+    background: none;
+    border: 0;
+    color: ${lighten(0.2, "#7159c1")};
+    padding: 0 5px;
+    margin: 0 5px;
+    border-left: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  ${props =>
+    props.unread &&
+    css`
+      &::after {
+        content: "";
+        color: red;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: red;
+        display: inline-block;
+      }
+    `}
+`;
+
+export const Scroll = styled(PerfectScrollbar)`
+  max-height: 260px;
+  padding: 5px 15px;
+`;
