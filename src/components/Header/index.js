b805b1bd9025313a525a19a2e7b6bb408603f207
Marcus Reaiche <marcusreaiche@gmail.com>
2020-02-17 22:00:43 -0300

    Updating Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import Notifications from "../Notifications";
 
@@ -7,6 +8,8 @@ import logo from "../../assets/logo-purple.svg";
 import { Container, Content, Profile } from "./styles";
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -18,12 +21,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Marcus Reaiche</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/face/4/3/0/00ff00"
-              alt="Marcus Reaiche"
+              src={
+                profile.avatar.url ||
+                "https://api.adorable.io/avatars/face/4/3/0/00ff00"
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
